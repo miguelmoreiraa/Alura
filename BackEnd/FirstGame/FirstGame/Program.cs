41bcf5c61ffe21bd28f79c971635c5ffbbe70458
miguelmoreiraa <miguel.moreira.mm44@gmail.com>
2024-09-10 18:18:33 -0300

    Changes in FirstProject and created First Game

---
+++ BackEnd/FirstGame/FirstGame/Program.cs
@@ -0,0 +1,31 @@
+﻿// First Game
+
+void GameNumbers()
+{
+    Random aleatorio = new Random();
+    int chosenNumber = aleatorio.Next(1, 101);
+
+    do {
+        Console.WriteLine("Digite um número");
+
+        int chute = int.Parse(Console.ReadLine());
+
+        if (chute == chosenNumber)
+        {
+            Console.WriteLine("Parabéns! Você acertou o número.");
+            break;
+        }
+        else if (chute < chosenNumber)
+        {
+            Console.WriteLine("O número é maior.");
+        }
+        else
+        {
+            Console.WriteLine("O número é menor.");
+        }
+    } while (true);
+
+    Console.WriteLine("O jogo acabou. Você acertou o número secreto!");
+}
+
+GameNumbers();
